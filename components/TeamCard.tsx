36a1b8dc68b5011a8aab1cb2d5e7b918ee9daf9a
hhsgr <hhsgr.club@gmail.com>
2022-07-27 01:04:21 +0900

    team image changed

--- components/TeamCard.tsx
+++ components/TeamCard.tsx
@@ -18,20 +18,10 @@ const TeamCard: FC<TeamCardProps> = ({
   color,
   animation,
 }) => {
-  const { colorMode } = useColorMode();
-
-  const { isAnimated, dom } = useAnimate();
-
   return (
     <Flex
       m={[8, 8, 8, 4]}
       // className={`${isAnimated && "animate__animated animate__" + animation}`}
-      ref={dom}
-      bgGradient={
-        colorMode === "light"
-          ? `linear(to-b, ${color}.500, ${color}.400, ${color}.300, ${color}.200)`
-          : `linear(to-b, ${color}.100, ${color}.300, ${color}.500, ${color}.700)`
-      }
       w={225}
       py={6}
       rounded="xl"
@@ -43,14 +33,14 @@ const TeamCard: FC<TeamCardProps> = ({
       <Image src={`../images/${image}`} w={175} rounded="full" alt="team" />
       <Text
         fontFamily={"NotoSansCJKkr"}
-        fontSize="30"
+        fontSize="2xl"
         mt={2}
         fontWeight="bold"
-        color={"c88be0"}
+        textColor="c88be0"
       >
         {name}
       </Text>
-      <Text mt={2} fontSize="20" color={"c88be0"}>
+      <Text mt={2} fontSize="lg" textColor="c88be0">
         {position}
       </Text>
     </Flex>
